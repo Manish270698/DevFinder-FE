1e2caa2951429a2820b41658d861381015765ab5
Manish Kumar <manishjune27@gmail.com>
2025-01-09 08:12:15 +0530

    updated pagination functionality

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:7777";
+export const BASE_URL = "/api";
 const ssh_command =
   'ssh -i "DevFinder-secret.pem" ubuntu@ec2-13-61-147-239.eu-north-1.compute.amazonaws.com';
