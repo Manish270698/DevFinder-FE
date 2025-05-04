62e018e69c78e8d52bf8b38bd79d4de1f14d76ea
Manish Kumar <manishjune27@gmail.com>
2025-05-05 03:27:55 +0530

    added chatting functionality

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://localhost:7777";
+// export const BASE_URL = "http://localhost:7777";
+export const BASE_URL = "https://devfinder-p7yr.onrender.com";
 const ssh_command =
   'ssh -i "DevFinder-secret.pem" ubuntu@ec2-13-61-147-239.eu-north-1.compute.amazonaws.com';
