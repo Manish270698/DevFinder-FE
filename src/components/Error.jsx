ec3631f2d319f74fca4ded985ca0134aaf0ad458
Manish Kumar <manishjune27@gmail.com>
2025-05-05 03:36:37 +0530

    added chatting functionality

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,6 @@
 const Error = () => {
   return (
-    <div className="min-h-dvh mt-16 flex justify-center">
+    <div className="min-h-dvh mt-16 flex justify-center text-brand-light">
       Something went wrong!
     </div>
   );
